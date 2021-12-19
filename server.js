fad1837a2d511e5870fbd0537319526ac4554c26
Krushil Naik <krushilnaik96@gmail.com>
2021-12-19 09:08:51 -0500

    Write server logic

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const {
+	parsed: { MONGODB_URI = '', PORT = 8080 }
+} = require('dotenv').config();
+
+const routes = require('./routes');
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+mongoose.connect(MONGODB_URI || 'mongodb://localhost/social-network');
+
+app.listen(PORT, () => {
+	console.log(`Server listening on port ${PORT}`);
+});
